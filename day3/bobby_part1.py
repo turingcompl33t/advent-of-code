9550615588f7fbb8e580cf4b9d4184fed98577bf
Bobby Nelson <bobby@localhost.localdomain>
2020-12-03 09:58:51 -0500

    Bobby: Day 3

---
+++ day3/bobby_part1.py
@@ -0,0 +1,25 @@
+# Advent of Code 2020, Day 3, Part 1
+# Author: Bobby Nelson
+
+def read_file(filename):
+    with open(filename) as file:
+        return [list(line.rstrip('\n')) for line in file]
+
+def calculate_trees(coordinates):
+    h_slope = 3 # move right 3 times
+    v_slope = 1 # move down 1 time
+    width = len(coordinates[0])
+    h_pos = 0
+    v_pos = 0
+    tree_counter = 0
+    while (v_pos < len(coordinates)):
+        if coordinates[v_pos][h_pos % width] == '#':
+            tree_counter = tree_counter + 1
+        h_pos = h_pos + h_slope
+        v_pos = v_pos + v_slope
+    return tree_counter
+    
+
+if __name__ == "__main__":
+    input = read_file("input.txt")
+    print(calculate_trees(input))

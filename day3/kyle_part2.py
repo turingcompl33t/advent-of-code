923f570f5eefde646570ddb84b09f440b1c02559
turingcompl33t <turingcompl33t@gmail.com>
2020-12-03 19:34:11 -0500

    just had to do one of these in c++

--- day3/kyle_part2.py
+++ day3/kyle_part2.py
@@ -16,7 +16,7 @@ def lines() -> str:
 def hits_for_slope(right: int, down: int) -> int:
     # perhaps not the most readable way to express this...
     # but I'll be damned if Robin can implement this more concisely in elixir
-    return sum([line[(i*right)%len(line)] == "#" for i, line in enumerate(itertools.islice(lines(), 0, None, down))])
+    return sum([line[(i*right)%len(line)] is "#" for i, line in enumerate(itertools.islice(lines(), 0, None, down))])
 
 def main():
     slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
